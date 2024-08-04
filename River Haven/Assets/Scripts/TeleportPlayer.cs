e1f3dca51722c41dd167804e515f965f0bc61e0f
Alexander Grönroos <alexandergronroos@gmail.com>
2024-08-04 10:58:18 +0300

    PlayerTeleportation added

---
+++ River Haven/Assets/Scripts/TeleportPlayer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayer : MonoBehaviour
+{
+    public Transform playerTransformation;
+    public Transform destination;
+    public GameObject playerObject;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("OnTriggerEnter called with " + other.gameObject.name);
+
+        if (other.CompareTag("Player"))
+        {
+            //Debug.Log("Player detected. Starting teleportation.");
+            
+            playerObject.SetActive(false);
+            //Debug.Log("Player object deactivated.");
+
+            playerTransformation.position = destination.position;
+            //Debug.Log("Player position set to destination: " + destination.position);
+
+            playerObject.SetActive(true);
+            //Debug.Log("Player object reactivated.");
+        }
+        else
+        {
+            Debug.Log("Collided object is not the player.");
+        }
+    }
+    
+
+    
+    
+}
