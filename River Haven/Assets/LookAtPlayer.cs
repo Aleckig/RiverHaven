68b8fb7d5e6da0030daf8ac3afaa1bf3d126bc65
KimiJokinen <kimi.jokinen@edu.turkuamk.fi>
2024-08-12 13:37:41 +0300

    Fixed dialogue
    NPC now turns towards player more smoothly when you talk to them. Fixed bug where character sometimes had running animation while talking to NPC.

---
+++ River Haven/Assets/LookAtPlayer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    public void TurnTowardsTarget(GameObject target)
+    {
+        StartCoroutine(SmoothLookAt(transform.rotation, Quaternion.LookRotation(target.transform.position - transform.position), 0.2f));
+    }
+
+    IEnumerator SmoothLookAt(Quaternion originalRotation, Quaternion
+            finalRotation, float duration)
+    {
+        if (duration > 0f)
+        {
+            float startTime = Time.time;
+            float endTime = startTime + duration;
+            transform.rotation = originalRotation;
+            yield return null;
+            while (Time.time < endTime)
+            {
+                float progress = (Time.time - startTime) / duration;
+                transform.rotation = Quaternion.Slerp(originalRotation,
+                finalRotation, progress);
+                yield return null;
+            }
+        }
+        transform.rotation = finalRotation;
+    }
+}
