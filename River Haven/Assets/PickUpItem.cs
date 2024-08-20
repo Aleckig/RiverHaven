50985b99a031426930c271b906f3c07497d91782
KimiJokinen <kimi.jokinen@edu.turkuamk.fi>
2024-08-20 19:28:30 +0300

    Added demos for quests and conditional dialogues
    Added box moving quest and birthday invite demos + quest log

---
+++ River Haven/Assets/PickUpItem.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour
+{
+    private Transform pickUpPoint;
+    private Transform player;
+    public float pickUpDistance;
+    public float force = 30f;
+    public bool readyToThrow;
+    public bool itemPickedUp;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.Find("TestPlayer").transform;
+        pickUpPoint = GameObject.Find("PickUpPoint").transform;
+    }
+
+    void Update()
+    {
+        pickUpDistance = Vector3.Distance(player.position, transform.position);
+
+        if (pickUpDistance <= 2f)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && itemPickedUp == false && pickUpPoint.childCount < 1)
+            {
+                rb.useGravity = false;
+                rb.constraints = RigidbodyConstraints.FreezeAll;
+                this.transform.parent = GameObject.Find("PickUpPoint").transform;
+                itemPickedUp = true;
+            }
+        }
+        if (itemPickedUp == true)
+        {
+            this.transform.position = pickUpPoint.position;
+        }
+        if (Input.GetKeyUp(KeyCode.Space) && itemPickedUp == true)
+        {
+            readyToThrow = true;
+            rb.AddForce(player.transform.forward * force);
+            this.transform.parent = null;
+            GetComponent<Rigidbody>().useGravity = true;
+            rb.constraints = RigidbodyConstraints.None;
+            itemPickedUp = false;
+            readyToThrow = false;
+        }
+    }
+}
