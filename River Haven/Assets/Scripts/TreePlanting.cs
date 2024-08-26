716d3cf31b635d19bce797f5d922c059e7cfb58e
Alexander Grönroos <alexandergronroos@gmail.com>
2024-08-26 10:31:54 +0300

    Tree Planting Mechanic added

--- River Haven/Assets/Scripts/TreePlanting.cs
+++ River Haven/Assets/Scripts/TreePlanting.cs
@@ -1,109 +1,167 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TreePlanting : MonoBehaviour
 {
-public GameObject questMarker;         // Quest marker child object
-    public GameObject dirt;                // Dirt child object
-    public GameObject smallTree;           // Small tree child object
-    public GameObject finalTree;           // Final tree child object
-    public GameObject dirtParticleEffect;  // Particle effect for digging
-    public GameObject waterParticleEffect; // Particle effect for watering
+    public GameObject questMarker;
+    public GameObject dirt;
+    public GameObject smallTree;
+    public GameObject finalTree;
+    public ParticleSystem dirtParticleEffect;
+    public ParticleSystem waterParticleEffect;
+    
+    private bool playerInArea = false;
+    private bool shovelInArea = false;
+    private bool plantInArea = false;
+    private bool bucketInArea = false;
+    
+    private int actionCount = 0;
+    private const int requiredActions = 5;
 
-    private bool hasShovel = false;
-    private bool hasPlant = false;
-    private bool hasBucket = false;
-    private bool inTreePlantingArea = false;
+    private enum PlantingStage
+    {
+        NotStarted,
+        Digging,
+        Planting,
+        Watering,
+        Completed
+    }
+
+    private PlantingStage currentStage = PlantingStage.NotStarted;
 
     void Start()
     {
-        // Initially, only the quest marker should be active
         dirt.SetActive(false);
         smallTree.SetActive(false);
         finalTree.SetActive(false);
+        Debug.Log("TreePlanting: Start method called. Initial stage: " + currentStage);
     }
 
     void Update()
     {
-        if (inTreePlantingArea && hasShovel && Input.GetKeyDown(KeyCode.E) && questMarker.activeSelf)
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            StartCoroutine(Dig());
-        }
-        else if (inTreePlantingArea && hasPlant && Input.GetKeyDown(KeyCode.E) && dirt.activeSelf)
-        {
-            StartCoroutine(PlantTree());
-        }
-        else if (inTreePlantingArea && hasBucket && Input.GetKeyDown(KeyCode.E) && smallTree.activeSelf)
-        {
-            StartCoroutine(WaterTree());
+            PerformAction();
         }
     }
 
-    private IEnumerator Dig()
+    private void PerformAction()
     {
-        // Disable the quest marker
-        questMarker.SetActive(false);
+        if (!playerInArea) return;
 
-        // Simulate digging by pressing E several times
-        for (int i = 0; i < 3; i++)
+        Debug.Log("TreePlanting: PerformAction called. Current stage: " + currentStage);
+        switch (currentStage)
         {
-            Instantiate(dirtParticleEffect, questMarker.transform.position, Quaternion.identity);
-            yield return new WaitForSeconds(0.5f);
+            case PlantingStage.NotStarted:
+                if (shovelInArea) StartDigging();
+                else Debug.Log("TreePlanting: Cannot start digging. No shovel in area.");
+                break;
+            case PlantingStage.Digging:
+                if (shovelInArea) ContinueDigging();
+                else Debug.Log("TreePlanting: Cannot continue digging. No shovel in area.");
+                break;
+            case PlantingStage.Planting:
+                if (plantInArea) ContinuePlanting();
+                else Debug.Log("TreePlanting: Cannot plant. No plant in area.");
+                break;
+            case PlantingStage.Watering:
+                if (bucketInArea) ContinueWatering();
+                else Debug.Log("TreePlanting: Cannot water. No bucket in area.");
+                break;
+            case PlantingStage.Completed:
+                Debug.Log("TreePlanting: Tree is already fully grown.");
+                break;
         }
-
-        // Activate the dirt object
-        dirt.SetActive(true);
     }
 
-    private IEnumerator PlantTree()
+    private void StartDigging()
     {
-        // Simulate planting by pressing E several times
-        for (int i = 0; i < 3; i++)
+        Debug.Log("TreePlanting: Starting to dig.");
+        currentStage = PlantingStage.Digging;
+        actionCount = 0;
+        ContinueDigging();
+    }
+
+    private void ContinueDigging()
+    {
+        actionCount++;
+        Debug.Log("TreePlanting: Digging. Action count: " + actionCount);
+        if (dirtParticleEffect != null)
         {
-            Instantiate(dirtParticleEffect, dirt.transform.position, Quaternion.identity);
-            yield return new WaitForSeconds(0.5f);
+            dirtParticleEffect.transform.position = questMarker.transform.position;
+            dirtParticleEffect.Play();
         }
-
-        // Activate the small tree and deactivate the dirt object
-        smallTree.SetActive(true);
-        dirt.SetActive(false);
-    }
-
-    private IEnumerator WaterTree()
-    {
-        // Simulate watering by pressing E several times
-        for (int i = 0; i < 3; i++)
+        
+        if (actionCount >= requiredActions)
         {
-            Instantiate(waterParticleEffect, smallTree.transform.position, Quaternion.identity);
-            yield return new WaitForSeconds(0.5f);
+            questMarker.SetActive(false);
+            dirt.SetActive(true);
+            actionCount = 0;
+            currentStage = PlantingStage.Planting;
+            Debug.Log("TreePlanting: Digging completed. Moving to Planting stage.");
         }
-
-        // Activate the final tree and deactivate the small tree
-        finalTree.SetActive(true);
-        smallTree.SetActive(false);
     }
 
-    public void PickUpShovel()
+    private void ContinuePlanting()
     {
-        hasShovel = true;
+        actionCount++;
+        Debug.Log("TreePlanting: Planting. Action count: " + actionCount);
+        if (dirtParticleEffect != null)
+        {
+            dirtParticleEffect.transform.position = dirt.transform.position;
+            dirtParticleEffect.Play();
+        }
+        
+        if (actionCount >= requiredActions)
+        {
+            dirt.SetActive(false);
+            smallTree.SetActive(true);
+            actionCount = 0;
+            currentStage = PlantingStage.Watering;
+            Debug.Log("TreePlanting: Planting completed. Moving to Watering stage.");
+        }
     }
 
-    public void PickUpPlant()
+    private void ContinueWatering()
     {
-        hasPlant = true;
-    }
-
-    public void PickUpBucket()
-    {
-        hasBucket = true;
+        actionCount++;
+        Debug.Log("TreePlanting: Watering. Action count: " + actionCount);
+        if (waterParticleEffect != null)
+        {
+            waterParticleEffect.transform.position = smallTree.transform.position;
+            waterParticleEffect.Play();
+        }
+        
+        if (actionCount >= requiredActions)
+        {
+            smallTree.SetActive(false);
+            finalTree.SetActive(true);
+            actionCount = 0;
+            currentStage = PlantingStage.Completed;
+            Debug.Log("TreePlanting: Watering completed. Tree fully grown.");
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            inTreePlantingArea = true;
+            playerInArea = true;
+            Debug.Log("TreePlanting: Player entered tree planting area.");
+        }
+        else if (other.CompareTag("Shovel"))
+        {
+            shovelInArea = true;
+            Debug.Log("TreePlanting: Shovel entered tree planting area.");
+        }
+        else if (other.CompareTag("Plant"))
+        {
+            plantInArea = true;
+            Debug.Log("TreePlanting: Plant entered tree planting area.");
+        }
+        else if (other.CompareTag("Bucket"))
+        {
+            bucketInArea = true;
+            Debug.Log("TreePlanting: Bucket entered tree planting area.");
         }
     }
 
@@ -111,7 +169,23 @@ public GameObject questMarker;         // Quest marker child object
     {
         if (other.CompareTag("Player"))
         {
-            inTreePlantingArea = false;
+            playerInArea = false;
+            Debug.Log("TreePlanting: Player exited tree planting area.");
+        }
+        else if (other.CompareTag("Shovel"))
+        {
+            shovelInArea = false;
+            Debug.Log("TreePlanting: Shovel exited tree planting area.");
+        }
+        else if (other.CompareTag("Plant"))
+        {
+            plantInArea = false;
+            Debug.Log("TreePlanting: Plant exited tree planting area.");
+        }
+        else if (other.CompareTag("Bucket"))
+        {
+            bucketInArea = false;
+            Debug.Log("TreePlanting: Bucket exited tree planting area.");
         }
     }
 }
