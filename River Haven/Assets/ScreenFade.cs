5dc5eaf8d49c6354579b6e2b269b34e139a14e13
KimiJokinen <kimi.jokinen@edu.turkuamk.fi>
2025-03-13 02:26:19 +0200

    Added first quest to act 2

--- River Haven/Assets/ScreenFade.cs
+++ River Haven/Assets/ScreenFade.cs
@@ -11,10 +11,24 @@ public class ScreenFade : MonoBehaviour
     //public string alertMessage = "This is an alert!"; // The message for the alert
 
     private bool isFading = false;
+    private bool showTheAlert = true;
+    private bool teleportPlayer = false;
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Transform destination;
 
     // Call this function to trigger the fade effect, stay black, and fade back in
     public void TriggerFadeAndAlert()
     {
+        if (!isFading)
+        {
+            StartCoroutine(FadeSequence());
+        }
+    }
+
+    public void TriggerFadeAndTeleportHome()
+    {
+        showTheAlert = false;
+        teleportPlayer = true;
         if (!isFading)
         {
             StartCoroutine(FadeSequence());
@@ -34,6 +48,7 @@ public class ScreenFade : MonoBehaviour
 
         // Trigger the alert using Lua scripting from the Dialogue System
         TriggerAlertLua();
+        TeleportPlayer();
 
         // Fade back to transparent (normal)
         yield return StartCoroutine(FadeToColor(new Color(0, 0, 0, 0))); // Fade back to transparent (alpha = 0)
@@ -69,6 +84,17 @@ public class ScreenFade : MonoBehaviour
     {
         // Assuming you have set up the Lua function in the Dialogue System
         // This will trigger a Lua statement that can be hooked into your alert system
-        DialogueManager.ShowAlert("You feel rested. Go talk to Mai, she has some information for you.");
+        if (showTheAlert == true)
+        {
+            DialogueManager.ShowAlert("You feel rested. Go talk to Mai, she has some information for you.");
+        }
+    }
+
+    private void TeleportPlayer()
+    {
+        if (teleportPlayer == true)
+        {
+            playerTransform.position = destination.position;
+        }
     }
 }
